a1276e8ac1dcfca501ae2002e6408ffe3e9abe37
mohammadrezatitan <mrjafari9818@gmail.com>
2017-02-12 04:30:16 -0800

    Update and rename beyond.sh to parsol.sh

--- parsol.sh
+++ parsol.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $HOME/BDReborn
+cd $HOME/parsol
 
 install() {
 	    cd tg
@@ -31,7 +31,7 @@ if [ ! -f ./tg/tgcli ]; then
     exit 1
 fi
    echo -e "\033[38;5;208m"
-   echo -e "     > BeyondTeam Source :D                        "
+   echo -e "     > parsol :D                       "
    echo -e "                                              \033[0;00m"
    echo -e "\e[36m"
    ./tg/tgcli -s ./bot/bot.lua $@
