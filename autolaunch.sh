edf080b8bbeef79854909ce9ad21bfcc4430b5ba
mohammad <mrjafari9818@gmail.com>
2017-04-01 19:10:38 -0700

    Add files via upload

---
+++ autolaunch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+COUNTER=1
+while(true) do
+./parsol.sh
+curl "https://api.telegram.org/bot[ToKeN]/sendmessage" -F "chat_id=308444837" -F "text=#NEWCRASH-#TeleBeyond-Reloaded-${COUNTER}-times"
+let COUNTER=COUNTER+1 
+done
