846a4ad9e17c03adc161dfa7ff9d1d36b60085b5
mohammad <mrjafari9818@gmail.com>
2017-04-06 03:41:51 -0700

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -1167,7 +1167,24 @@ return adminlist(msg)
      if matches[1] == 'ترک' and is_admin(msg) then
   tdcli.changeChatMemberStatus(msg.to.id, our_id, 'Left', dl_cb, nil)
    end
-     if matches[1] == 'autoleave' and is_admin(msg) then
+if matches[1] == 'autoleave' and is_admin(msg) then
+local hash = 'auto_leave_bot'
+—Enable Auto Leave
+     if matches[2] == 'enable' then
+    redis:del(hash)
+   return 'Auto leave has been enabled'
+—Disable Auto Leave
+     elseif matches[2] == 'disable' then
+    redis:set(hash, true)
+   return 'Auto leave has been disabled'
+—Auto Leave Status
+      elseif matches[2] == 'status' then
+      if not redis:get(hash) then
+   return 'Auto leave is enable'
+       else
+   return 'Auto leave is disable'
+         end
+      end
    end
 
 
